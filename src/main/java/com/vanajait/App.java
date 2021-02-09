4da2878a2e79c42ed4dc6a998a2af767186dc8de
vanaja ch <you@example.com>
2021-02-09 11:27:09 +0530

    first commit

---
+++ src/main/java/com/vanajait/App.java
@@ -0,0 +1,20 @@
+package com.vanajait;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        System.out.println("total value is ..." +c);
+        
+        System.out.println("This is Maven Application");
+    }
+}
